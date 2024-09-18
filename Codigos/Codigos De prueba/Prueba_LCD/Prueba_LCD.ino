5598414e7e1ad4b747f26f639cce27b99bc23402
EituCher18 <eitancherniak@gmail.com>
2024-09-18 09:36:09 -0300

    Update Prueba_LCD.ino

--- Codigos/Codigos De prueba/Prueba_LCD/Prueba_LCD.ino
+++ Codigos/Codigos De prueba/Prueba_LCD/Prueba_LCD.ino
@@ -2,11 +2,11 @@
 #include <LiquidCrystal_I2C.h>  // Incluye la librería LiquidCrystal_I2C
 #include <Arduino.h>
 
-//#include <AHTxx.h>
+#include <AHTxx.h>
 
-//float AHT_Tem;
-//float AHT_Hum;
-//AHTxx aht10(AHTXX_ADDRESS_X38, AHT1x_SENSOR);
+float AHT_Tem;
+float AHT_Hum;
+AHTxx aht10(AHTXX_ADDRESS_X38, AHT1x_SENSOR);
 
 // Dirección I2C de la pantalla LCD
 int lcdAddress = 0x27;  // Puede variar según tu módulo I2C
@@ -44,25 +44,25 @@ void Codigo_Pruba() {
   lcd.print("Prieto Me aproba");
 }
 
-// void Codigo_Pruba_Con_AHT() {
+void Codigo_Pruba_Con_AHT() {
 
-//   AHT_Tem = aht10.readTemperature();
-//   lcd.setCursor(0, 0);
-//   lcd.print("La Temp es");
-//   lcd.setCursor(11, 0);
-//   lcd.print(AHT_Tem);
-//   lcd.setCursor(15, 0);
-//   lcd.print("C");
-//   AHT_Hum = aht10.readHumidity();
-//   lcd.setCursor(0, 1);
-//   lcd.print("La Hum es");
-//   lcd.setCursor(10, 1);
-//   lcd.print(AHT_Hum);
-//   lcd.setCursor(14, 1);
-//   lcd.print("%");
+  AHT_Tem = aht10.readTemperature();
+  lcd.setCursor(0, 0);
+  lcd.print("La Temp es");
+  lcd.setCursor(11, 0);
+  lcd.print(AHT_Tem);
+  lcd.setCursor(15, 0);
+  lcd.print("C");
+  AHT_Hum = aht10.readHumidity();
+  lcd.setCursor(0, 1);
+  lcd.print("La Hum es");
+  lcd.setCursor(10, 1);
+  lcd.print(AHT_Hum);
+  lcd.setCursor(14, 1);
+  lcd.print("%");
 
-//   if (millis() - Ms >= 1000) {
-//     Ms = millis();
-//     lcd.clear();
-//   }
-// }
+  if (millis() - Ms >= 1000) {
+    Ms = millis();
+    lcd.clear();
+  }
+}
